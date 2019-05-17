25ca7dcb0c26e856060336a50f3830efa5452e2a
Andre Toll <andretoll@outlook.com>
2019-05-18 00:32:15 +0200

    - Added settings functionality - Added settings themes and accents

--- Tolldo/Helpers/ThemeManager.cs
+++ Tolldo/Helpers/ThemeManager.cs
@@ -15,12 +15,16 @@ namespace Tolldo.Helpers
         #region Private Members
 
         private bool _darkThemeEnabled;
+        private List<string> _accents;
+        private string _accent;
 
         #endregion
 
         #region Public Properties
 
         public bool DarkThemeEnabled { get { return _darkThemeEnabled; } private set { _darkThemeEnabled = value; } }
+        public List<string> Accents { get { return _accents; } private set { _accents = value; } }
+        public string Accent { get { return _accent; } private set { _accent = value; } }
 
         #endregion
 
@@ -32,7 +36,12 @@ namespace Tolldo.Helpers
         public ThemeManager()
         {
             // Get settings
-            SetTheme(true);
+            DarkThemeEnabled = (bool)LoadSetting(Setting.DarkTheme.ToString());
+            Accent = (string)LoadSetting(Setting.Accent.ToString());
+
+            // Apply settings
+            SetTheme(DarkThemeEnabled);
+            SetAccent(Accent);
         }
 
         #endregion
@@ -80,7 +89,47 @@ namespace Tolldo.Helpers
 
                 // Set inverted
                 SetBrush("Dark", "InvertedBackgroundBrush");
-            }            
+            }
+
+            // Save setting
+            SaveSetting(Setting.DarkTheme.ToString(), DarkThemeEnabled);
+        }
+
+        /// <summary>
+        /// Method to set accent.
+        /// </summary>
+        /// <param name="accent"></param>
+        public void SetAccent(string accent)
+        {
+            Accent = accent;
+
+            switch(accent)
+            {
+                case "Blue":
+                    SetBrush("Blue", "ColorForegroundBrush");
+                    SetBrush("DarkerBlue", "DarkerColorForegroundBrush");
+                    SetBrush("DarkBlue", "DarkColorForegroundBrush");
+                    SetLinearBrush("DarkerBlue", "Blue", "ColorGradientBrush");
+                    break;
+                case "Red":
+                    SetBrush("Red", "ColorForegroundBrush");
+                    SetBrush("DarkerRed", "DarkerColorForegroundBrush");
+                    SetBrush("DarkRed", "DarkColorForegroundBrush");
+                    SetLinearBrush("DarkerRed", "Red", "ColorGradientBrush");
+                    break;
+                case "Orange":
+                    SetBrush("Orange", "ColorForegroundBrush");
+                    SetBrush("DarkerOrange", "DarkerColorForegroundBrush");
+                    SetBrush("DarkOrange", "DarkColorForegroundBrush");
+                    SetLinearBrush("DarkerOrange", "Orange", "ColorGradientBrush");
+                    break;
+                default:
+                    SetBrush("Blue", "ColorForegroundBrush");
+                    SetBrush("DarkerBlue", "DarkerColorForegroundBrush");
+                    SetBrush("DarkBlue", "DarkColorForegroundBrush");
+                    SetLinearBrush("DarkerBlue", "Blue", "ColorGradientBrush");
+                    break;
+            }
         }
 
         #endregion
@@ -99,6 +148,61 @@ namespace Tolldo.Helpers
             Application.Current.Resources[brushName] = brush;
         }
 
+        /// <summary>
+        /// Method to assign the specified <see cref="Color"/>s to the specified <see cref="LinearGradientBrush"/>.
+        /// </summary>
+        /// <param name="color1"></param>
+        /// <param name="color2"></param>
+        /// <param name="brushName"></param>
+        private void SetLinearBrush(string color1, string color2, string brushName)
+        {
+            Color firstColor = (Color)Application.Current.Resources[color1];
+            Color secondColor = (Color)Application.Current.Resources[color2];
+
+            LinearGradientBrush brush = new LinearGradientBrush();
+            brush.StartPoint = new Point(0, 0);
+            brush.EndPoint = new Point(1, 1);
+            brush.GradientStops.Add(new GradientStop(firstColor, 0.0));
+            brush.GradientStops.Add(new GradientStop(secondColor, 0.50));
+            brush.GradientStops.Add(new GradientStop(firstColor, 1.0));
+
+            Application.Current.Resources[brushName] = brush;
+        }
+
+        /// <summary>
+        /// Method to save the specific setting with the specific value.
+        /// </summary>
+        /// <param name="property"></param>
+        /// <param name="value"></param>
+        private void SaveSetting(string property, object value)
+        {
+            Properties.Settings.Default[property] = value;
+            Properties.Settings.Default.Save();
+        }
+
+        /// <summary>
+        /// Method to load the specific setting.
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        private object LoadSetting(string property)
+        {
+            return Properties.Settings.Default[property];
+        }
+
         #endregion
+
+        private enum Setting
+        {
+            DarkTheme,
+            Accent
+        }
+
+        private enum AccentRotation
+        {
+            Blue = 1,
+            Red = 2,
+            Orange = 3
+        }
     }
 }
--- Tolldo/Properties/Settings.Designer.cs
+++ Tolldo/Properties/Settings.Designer.cs
@@ -8,22 +8,42 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Tolldo.Properties
-{
-
-
+namespace Tolldo.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool DarkTheme {
+            get {
+                return ((bool)(this["DarkTheme"]));
+            }
+            set {
+                this["DarkTheme"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Orange")]
+        public string Accent {
+            get {
+                return ((string)(this["Accent"]));
+            }
+            set {
+                this["Accent"] = value;
             }
         }
     }
