d30190a764198ba3282270d7084e362dd97838ba
Andre Toll <andretoll@outlook.com>
2019-05-20 12:38:51 +0200

    - Added demo - Updated assembly info

--- Tolldo/Properties/AssemblyInfo.cs
+++ Tolldo/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Tolldo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A comprehensive and modern To-do application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Toll")]
 [assembly: AssemblyProduct("Tolldo")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -53,3 +53,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
