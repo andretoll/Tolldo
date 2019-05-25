78b02b8298de1fe59d73099ccdb3dcde3eb0042c
Andre Toll <andretoll@outlook.com>
2019-05-25 16:40:55 +0200

    - Added vertical scroll to visibility converter + reverse - Improved 'new task' design and usability

---
+++ Tolldo/ValueConverters/ReversedVerticalScrollToVisibilityConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Tolldo.ValueConverters
+{
+    /// <summary>
+    /// A value converter that returns <see cref="Visibility.Collapsed"/> if scrollbar is at the bottom.
+    /// </summary>
+    public class ReversedVerticalScrollToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Visibility)values[2] == Visibility.Visible)
+            {
+                return Visibility.Collapsed;
+            }
+
+            if ((double)values[0] != (double)values[1])
+            {
+                return Visibility.Collapsed;
+            }
+            else return Visibility.Visible;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tolldo/ValueConverters/VerticalScrollToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Tolldo.ValueConverters
+{
+    /// <summary>
+    /// A value converter that returns <see cref="Visibility.Visible"/> if scrollbar is at the bottom.
+    /// </summary>
+    public class VerticalScrollToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((double)values[0] == (double)values[1])
+            {
+                return Visibility.Collapsed;
+            }
+            else return Visibility.Visible;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
